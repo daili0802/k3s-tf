67af9dde4cc80bfa5f5b9a9278fd134aeb92879c
Li Dai <lidai@Other-Mac.hsd1.mn.comcast.net>
2025-02-20 23:05:19 -0600

    initial commit

---
+++ multipass/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_provider {
+    multipass = {
+      source = "larstobi/multipass"
+    }
+  }
+}
+
+provider "multipass" {}
+
+resource "multipass_instance" "multipass_vm" {
+  count  = 4
+  cloudinit_file  = ./cloud_init.cfg
+  name   = "k3s-${count.index + 1}"
+  cpus   = 2
+  memory = 4G
+  disk   = 20G
+  image  = "jammy"
+}
