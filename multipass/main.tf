db186b610dd162daf3eb9edabed011f4e3697b61
daili0802 <daili0802@gmail.com>
2025-03-01 13:43:14 -0600

    fix syntax error

--- multipass/main.tf
+++ multipass/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_provider {
+  required_providers {
     multipass = {
       source = "larstobi/multipass"
     }
@@ -10,10 +10,10 @@ provider "multipass" {}
 
 resource "multipass_instance" "multipass_vm" {
   count  = 4
-  cloudinit_file  = ./cloud_init.cfg
+  cloudinit_file  = cloud_init.cfg
   name   = "k3s-${count.index + 1}"
   cpus   = 2
-  memory = 4G
-  disk   = 20G
+  memory = "4G"
+  disk   = "20G"
   image  = "jammy"
 }
